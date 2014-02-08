2c036490880b8b5add7ed1c5c9b7d4965bb4c927
baconstarvation <ant.ucd@gmail.com>
2014-02-07 22:00:04 -0800

    added mongolab db to models

--- models/cats.js
+++ models/cats.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || 'localhost');
+mongoose.connect('mongodb://heroku_app22022794:onbv16pec74df9bknirkhao0qj@ds027789.mongolab.com:27789/heroku_app22022794' || 'localhost');
 
 var catSchema = mongoose.Schema({
   age: Number,
@@ -9,7 +9,6 @@ var catSchema = mongoose.Schema({
 
 var Cats = mongoose.model('Cats', catSchema);
 module.exports = Cats;
-
 
 
 
